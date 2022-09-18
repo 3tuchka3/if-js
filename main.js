45752e008d76bf60c56a0296fcf615df8331afaf
3tuchka3 <enotostated2.0@mail.ru>
2022-09-18 11:25:12 +0300

    fixed some in lesson-2

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ test += '1';
 console.log(test);
 console.log(Boolean(test));
 
-let mass = [2, 3, 5, 8];
+const mass = [2, 3, 5, 8];
 console.log(mass);
 let n = 1;
 for (let i = 0; i < 4; i++) {
@@ -19,17 +19,17 @@ for (let i = 0; i < 4; i++) {
 }
 console.log(n);
 
-mass1 = [2, 5, 8, 15, 0, 6, 20, 3];
+const mass1 = [2, 5, 8, 15, 0, 6, 20, 3];
 console.log(mass1);
-for (let i = 0; i<6; i++) {
+for (let i = 0; i < 6; i++) {
   if (mass1[i] > 5 && mass1[i] < 10) {
     console.log(mass1[i]);
   }
 }
 
 console.log(mass1);
-for (let i = 0; i<mass1.length; i++) {
-  if (mass1[i] % 2 == 0 && mass1[i]!= 0) {
+for (let i = 0; i < mass1.length; i++) {
+  if (mass1[i] % 2 == 0 && mass1[i] != 0) {
     console.log(mass1[i]);
   }
 }
