665ade55463e3c98e7fbf2f7347cad1f5dbf21b4
3tuchka3 <enotostated2.0@mail.ru>
2022-09-18 13:05:24 +0300

    add 1<mass.length

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ console.log(Boolean(test));
 const mass = [2, 3, 5, 8];
 console.log(mass);
 let n = 1;
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < mass.length; i++) {
   n *= mass[i];
 }
 console.log(n);
 
 const mass1 = [2, 5, 8, 15, 0, 6, 20, 3];
 console.log(mass1);
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < mass1.length; i++) {
   if (mass1[i] > 5 && mass1[i] < 10) {
     console.log(mass1[i]);
   }
