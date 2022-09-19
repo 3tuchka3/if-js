a08cd6a3907dbc756577ab9ec6132a5e88bb2c93
3tuchka3 <enotostated2.0@mail.ru>
2022-09-19 18:26:41 +0300

    add Array function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let x = 'qwerewq';
+const x = 'qwerewq';
 console.log(x);
 
 function palindrome(x) {
@@ -15,8 +15,8 @@ function palindrome(x) {
 
 console.log(palindrome(x));
 
-let numb1 = 10;
-let numb2 = 123;
+const numb1 = 10;
+const numb2 = 123;
 
 function min(x, y) {
   if (x < y) console.log('min = ', x);
@@ -35,22 +35,24 @@ function max(x, y) {
 max(numb1, numb2);
 
 
-
-let array = [];
+function array(){
+const array = [];
 
 for (let i = 0; i < 10; i++) {
   array.push(Math.round(Math.random() * (100 - 0) + 0));
 }
 for (let i = 0; i < 10; i++) {
   if (array[i] === 100) {
-    array[i] = array[i]/100 + 'zerozero'
+    array[i] = array[i] / 100 + 'zerozero';
   }
-  if (array[i]>10 && array[i] % 10 === 0){
-    array[i] = array[i]/10 + 'zero'
+  if (array[i] > 10 && array[i] % 10 === 0) {
+    array[i] = array[i] / 10 + 'zero';
   }
-  if (array[i]<10 && array[i] % 10 === 0){
-    array[i] = 'zero'
+  if (array[i] < 10 && array[i] % 10 === 0) {
+    array[i] = 'zero';
   }
 }
 console.log(array);
+}
 
+array();
