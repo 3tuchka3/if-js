af7ff8c33cd5f58aa39aeb4d24734d2f7ab1bdd1
Vadim <88191111+3tuchka3@users.noreply.github.com>
2022-11-29 08:21:19 +0300

    finished lesson-8 (#9)

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const obj1 = {
   a: 'a',
   b: {
@@ -52,7 +53,7 @@ function deepEqual(object1, object2) {
 console.log(deepEqual(obj1, obj2));
 console.log(deepEqual(obj1, obj3));
 
-/*
+/!*
 
 
 function palindrome(str) {
@@ -288,6 +289,89 @@ console.log(hotelsByCountries);
 
 
 
+*!/
+
+
 */
+class User {
+    constructor({ firstName, lastName }) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Student extends User {
+    constructor({ admissionYear, courseName, firstName, lastName }) {
+        super({ firstName, lastName });
+        this.admissionYear = admissionYear;
+        this.courseName = courseName;
+    }
+
+    get course() {
+        const now = new Date();
+        return now.getFullYear() - this.admissionYear;
+    }
+}
 
 
+class Students {
+    constructor(studentsData) {
+        this.studentsData = studentsData.map(
+            (prop) =>
+                new Student({
+                    admissionYear: prop.admissionYear,
+                    courseName: prop.courseName,
+                    firstName: prop.firstName,
+                    lastName: prop.lastName,
+                }),
+        );
+    }
+
+    getInfo() {
+        const result = [];
+        function byField(field) {
+            return (a, b) => (a[field] < b[field] ? 1 : -1);
+        }
+        this.studentsData.sort(byField('admissionYear'));
+        for (let i = 0; i < this.studentsData.length; i++) {
+            result.push(
+                `${this.studentsData[i].fullName} - ${this.studentsData[i].courseName}, ${this.studentsData[i].course} курс`,
+            );
+        }
+        return result;
+    }
+}
+
+const studentsData = [
+    {
+        firstName: 'Василий',
+        lastName: 'Петров',
+        admissionYear: 2019,
+        courseName: 'Java',
+    },
+    {
+        firstName: 'Иван',
+        lastName: 'Иванов',
+        admissionYear: 2018,
+        courseName: 'JavaScript',
+    },
+    {
+        firstName: 'Александр',
+        lastName: 'Федоров',
+        admissionYear: 2017,
+        courseName: 'Python',
+    },
+    {
+        firstName: 'Николай',
+        lastName: 'Петров',
+        admissionYear: 2019,
+        courseName: 'Android',
+    },
+];
+
+const std1 = new Students(studentsData);
+console.log(std1.getInfo());
