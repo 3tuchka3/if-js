b4d1a66eabfd15b72eaaf6fb51e2f15c02a8e2c4
Vadim <88191111+3tuchka3@users.noreply.github.com>
2023-02-07 21:50:44 +0300

    finished lesson-9 (#10)

--- main.js
+++ main.js
@@ -293,85 +293,123 @@ console.log(hotelsByCountries);
 
 
 */
-class User {
-    constructor({ firstName, lastName }) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+// class User {
+//   constructor({ firstName, lastName }) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+//
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
+//
+// class Student extends User {
+//   constructor({ admissionYear, courseName, firstName, lastName }) {
+//     super({ firstName, lastName });
+//     this.admissionYear = admissionYear;
+//     this.courseName = courseName;
+//   }
+//
+//   get course() {
+//     const now = new Date();
+//     return now.getFullYear() - this.admissionYear;
+//   }
+// }
+//
+// class Students {
+//   constructor(studentsData) {
+//     this.studentsData = studentsData.map(
+//       (prop) =>
+//         new Student({
+//           admissionYear: prop.admissionYear,
+//           courseName: prop.courseName,
+//           firstName: prop.firstName,
+//           lastName: prop.lastName,
+//         })
+//     );
+//   }
+//
+//   getInfo() {
+//     const result = [];
+//     function byField(field) {
+//       return (a, b) => (a[field] < b[field] ? 1 : -1);
+//     }
+//     this.studentsData.sort(byField('admissionYear'));
+//     for (let i = 0; i < this.studentsData.length; i++) {
+//       result.push(
+//         `${this.studentsData[i].fullName} - ${this.studentsData[i].courseName}, ${this.studentsData[i].course} курс`
+//       );
+//     }
+//     return result;
+//   }
+// }
+//
+// const studentsData = [
+//   {
+//     firstName: 'Василий',
+//     lastName: 'Петров',
+//     admissionYear: 2019,
+//     courseName: 'Java',
+//   },
+//   {
+//     firstName: 'Иван',
+//     lastName: 'Иванов',
+//     admissionYear: 2018,
+//     courseName: 'JavaScript',
+//   },
+//   {
+//     firstName: 'Александр',
+//     lastName: 'Федоров',
+//     admissionYear: 2017,
+//     courseName: 'Python',
+//   },
+//   {
+//     firstName: 'Николай',
+//     lastName: 'Петров',
+//     admissionYear: 2019,
+//     courseName: 'Android',
+//   },
+// ];
+//
+// const std1 = new Students(studentsData);
+// console.log(std1.getInfo());
 
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
-
-class Student extends User {
-    constructor({ admissionYear, courseName, firstName, lastName }) {
-        super({ firstName, lastName });
-        this.admissionYear = admissionYear;
-        this.courseName = courseName;
-    }
-
-    get course() {
-        const now = new Date();
-        return now.getFullYear() - this.admissionYear;
-    }
-}
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
 
 
-class Students {
-    constructor(studentsData) {
-        this.studentsData = studentsData.map(
-            (prop) =>
-                new Student({
-                    admissionYear: prop.admissionYear,
-                    courseName: prop.courseName,
-                    firstName: prop.firstName,
-                    lastName: prop.lastName,
-                }),
-        );
-    }
-
-    getInfo() {
-        const result = [];
-        function byField(field) {
-            return (a, b) => (a[field] < b[field] ? 1 : -1);
+const colors = {
+    data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+    current: 0,
+    [Symbol.iterator]() {
+        return this;
+    },
+    next() {
+        if (this.current < this.data.length) {
+            return {
+                done: false,
+                value: this.data[this.current++],
+            }
         }
-        this.studentsData.sort(byField('admissionYear'));
-        for (let i = 0; i < this.studentsData.length; i++) {
-            result.push(
-                `${this.studentsData[i].fullName} - ${this.studentsData[i].courseName}, ${this.studentsData[i].course} курс`,
-            );
+        if (this.current <= this.data.length) {
+            this.current = 0;
+            return {
+                done: false,
+                value: this.data[this.current],
+            };
         }
-        return result;
+    },
+};
+
+function setColor(colors){
+    return function (event){
+        event.target.style.color = colors.next().value;
+        return event;
     }
 }
 
-const studentsData = [
-    {
-        firstName: 'Василий',
-        lastName: 'Петров',
-        admissionYear: 2019,
-        courseName: 'Java',
-    },
-    {
-        firstName: 'Иван',
-        lastName: 'Иванов',
-        admissionYear: 2018,
-        courseName: 'JavaScript',
-    },
-    {
-        firstName: 'Александр',
-        lastName: 'Федоров',
-        admissionYear: 2017,
-        courseName: 'Python',
-    },
-    {
-        firstName: 'Николай',
-        lastName: 'Петров',
-        admissionYear: 2019,
-        courseName: 'Android',
-    },
-];
-
-const std1 = new Students(studentsData);
-console.log(std1.getInfo());
+text1.addEventListener('click',setColor({...colors}));
+text2.addEventListener('click',setColor({...colors}));
+text3.addEventListener('click',setColor({...colors}));
